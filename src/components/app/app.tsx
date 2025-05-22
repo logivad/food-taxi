562543cc1fd3067dc16c39c0d42a2165ac80480b
Vadim Loginov <logivad@ya.ru>
2025-05-22 09:57:23 +0300

    Minor refactoring

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,12 @@
 import { type FC } from 'react';
 
 import { Layout } from '../layout/layout';
-import { MainPage } from '../pages/main-page/main-page';
+import { RestaurantsPage } from '../pages/main-page/main-page';
 
 export const App: FC = () => {
   return (
     <Layout>
-      <MainPage />
+      <RestaurantsPage />
     </Layout>
   );
 };
--- src/components/pages/main-page/main-page.tsx
+++ src/components/pages/main-page/main-page.tsx
@@ -4,7 +4,7 @@ import { RestaurantsTabs } from '../../nav/nav';
 import { RestaurantCard } from '../../restaurant/restaurant-card';
 import restaurantsMock from '../../../../mocks/restaurants';
 
-export const MainPage: FC = () => {
+export const RestaurantsPage: FC = () => {
   const [restaurants] = useState(restaurantsMock);
   const [activeRestaurant, setActiveRestaurant] = useState(restaurants[0]);
 
