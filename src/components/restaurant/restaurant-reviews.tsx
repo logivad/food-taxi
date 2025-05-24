c807f808450234f7279ad0e8e589abd283ccfa6d
Vadim Loginov <logivad@ya.ru>
2025-05-24 12:15:26 +0300

    show placeholder if no reviews

--- src/components/restaurant/restaurant-reviews.tsx
+++ src/components/restaurant/restaurant-reviews.tsx
@@ -4,14 +4,20 @@ import type { Review } from '../../model/restaurant';
 export const RestaurantReviews: FC<{ reviews: Array<Review> }> = ({
   reviews,
 }) => {
+  const reviewList = () => (
+    <ul>
+      {reviews.map(({ id, text }) => (
+        <li key={id}>{text}</li>
+      ))}
+    </ul>
+  );
+
+  const noReviewsPlaceholder = () => <p>Отзывов пока нет</p>;
+
   return (
     <div>
       <h3>Отзывы</h3>
-      <ul>
-        {reviews.map(({ id, text }) => (
-          <li key={id}>{text}</li>
-        ))}
-      </ul>
+      {reviews.length ? reviewList() : noReviewsPlaceholder()}
       <hr />
     </div>
   );
