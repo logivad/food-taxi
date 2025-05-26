32f023313eae0f7ada41561e30c3b81bcf49f14c
Vadim Loginov <logivad@ya.ru>
2025-05-26 08:00:23 +0300

    Add TailwindCSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot, type Root } from 'react-dom/client';
 
 import { App } from './components/app/app';
 import { StrictMode } from 'react';
+
+declare global {
+  interface Window {
+    reactRoot: Root;
+  }
+}
 
 const root = document.getElementById('root');
 
 if (root) {
   const reactRoot = createRoot(root);
+  window.reactRoot = reactRoot;
 
   reactRoot.render(
     <StrictMode>
